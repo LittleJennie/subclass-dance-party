a778a67e4a0face2efc5f06da3865960ac4a1965
littlejennie <jenniezeng1@gmail.com>
2019-06-03 20:26:55 -0700

    set up two dancers

---
+++ src/happyDancer.js
@@ -0,0 +1,19 @@
+var makeHappyDancer = function(top, left, timeBetweenSteps) {
+  //var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+  makeDancer.call(this, top, left, timeBetweenSteps);
+  this.$node.addClass('happydancer');
+  // this.$node.removeClass('dancer');
+};
+
+makeHappyDancer.prototype = Object.create(makeDancer.prototype);
+makeHappyDancer.prototype.constructor = makeHappyDancer;
+makeHappyDancer.prototype.step = function() {
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+  // call the old version of step at the beginning of any call to this new version of step
+  makeDancer.prototype.step.call(this);
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  this.$node.toggle();
+};
